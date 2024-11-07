6bf3db1d59f8a672fa261d4c2faee5674a2598c1
EduardooSodre <edduardooo2011@hotmail.com>
2024-11-07 20:33:06 -0300

    feat: add login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} dark`}>
-        <h1>layout</h1>
         {children}
       </body>
     </html>
