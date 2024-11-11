1b6a4242d7fa693e40973354423fd5e3a8e3444e
EduardooSodre <edduardooo2011@hotmail.com>
2024-11-11 00:56:53 -0300

    feat: add summary-card

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import Navbar from "./_components/navbar";
+import Navbar from "../_components/navbar";
+import SummarryCards from "./_components/summary-cards";
 
 const Home = async () => {
   const { userId } = await auth();
@@ -11,6 +12,11 @@ const Home = async () => {
   if (!userId) {
     redirect("/login");
   }
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <SummarryCards />
+    </>
+  );
 };
 export default Home;
