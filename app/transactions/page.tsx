1ac610cb75a869a25eb9c2778fbfcaa6568bd28f
EduardooSodre <edduardooo2011@hotmail.com>
2024-11-11 00:17:58 -0300

    feat: add navbar component

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -2,19 +2,23 @@ import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { TransactionColumns } from "./_columns";
 import AddtransactionButton from "../_components/add-transaction-button";
+import Navbar from "../_components/navbar";
 
 const TransactionPage = async () => {
   //irá acessar as trasações do meu banco de dados com prisma
   const transactions = await db.transaction.findMany({});
   return (
-    <div className="space-y-6 p-6">
-      {/* TITULO E BOTÃO */}
-      <div className="w=full flex items-center justify-between">
-        <h1 className="text-2xl font-bold">Transações</h1>
-        <AddtransactionButton />
+    <>
+      <Navbar />
+      <div className="space-y-6 p-6">
+        {/* TITULO E BOTÃO */}
+        <div className="w=full flex items-center justify-between">
+          <h1 className="text-2xl font-bold">Transações</h1>
+          <AddtransactionButton />
+        </div>
+        <DataTable columns={TransactionColumns} data={transactions} />
       </div>
-      <DataTable columns={TransactionColumns} data={transactions} />
-    </div>
+    </>
   );
 };
 
