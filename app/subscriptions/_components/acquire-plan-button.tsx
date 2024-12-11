62295c33fcd5b16f794fa956b23a670b21d73473
EduardooSodre <edduardooo2011@hotmail.com>
2024-12-11 15:59:34 -0300

    feat: add stripe wenhook that handles subscriptions

--- app/subscriptions/_components/acquire-plan-button.tsx
+++ app/subscriptions/_components/acquire-plan-button.tsx
@@ -3,12 +3,15 @@
 import { Button } from "@/app/_components/ui/button";
 import { createStripeCheckout } from "../_actions/create-stripe-checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 const AcquirePlanButton = () => {
+  const { user } = useUser();
   const handleAcquirePlanClick = async () => {
     const { sessionId } = await createStripeCheckout();
     if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
-      throw new Error("Stripe publishble key not found");
+      throw new Error("Stripe publishable key not found");
     }
     const stripe = await loadStripe(
       process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
@@ -19,6 +22,18 @@ const AcquirePlanButton = () => {
     await stripe.redirectToCheckout({ sessionId });
   };
 
+  const hasPlatinumPlan = user?.publicMetadata.subscriptionPlan == "platinum";
+  if (hasPlatinumPlan) {
+    return (
+      <Button className="w-full rounded-full font-bold" variant="link">
+        <Link
+          href={`${process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_URL as string}?prefilled_email=${user.emailAddresses[0].emailAddress}`}
+        >
+          Gerenciar plano
+        </Link>
+      </Button>
+    );
+  }
   return (
     <Button
       className="w-full rounded-full font-bold"
